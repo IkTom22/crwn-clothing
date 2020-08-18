a19bde009dfe599a0b7970be6608a88da41317d0
IkTom22 <ikotominaga@gmail.com>
2020-08-19 08:04:55 +1200

    broken our homepage intgo smaller conponents, finished styling our menu item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../directory/directory.component';
+import './homepage.styles.scss';
+
+// no state or lifesycle methods so just function base component
+const HomePage = ()=>(
+   <div className='homepage'>
+        <Directory />
+   </div>
+)
+export default HomePage;
