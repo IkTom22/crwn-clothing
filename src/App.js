508fa5cc348f30f8eb46ad9999fce23305255ec9
IkTom22 <ikotominaga@gmail.com>
2020-08-31 14:07:15 +1200

    implemeted firebase utils, included ability to store authenticated users into our firebasestore database

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+//import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 class App extends React.Component {
   constructor(props){
     super(props);
@@ -19,9 +19,25 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
-      this.setState({ currentUser: user });
-      console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+
+          console.log(this.state);
+        });
+       
+      }
+      
+      this.setState({ currentUser: userAuth });
+      
     })
   }
   componentWillUnmount(){
@@ -30,7 +46,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
-      <Header currentUSer={this.state.currentUser}/>
+      <Header currentUser={this.state.currentUser}/>
         <Switch>
             <Route  exact path='/' component={ HomePage }/>
             <Route  exact path='/shop' component={ShopPage} />
---
+++ src/test.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('3WSHxOOsXvurs7hpWVkL').collection('May')
+firestore.doc('/user/3WSHxOOsXvurs7hpWVkL/May')
