05f3581386524508f18279e87219e7414271aea0
IkTom22 <ikotominaga@gmail.com>
2020-10-30 15:55:29 +1300

    server bug fixed

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
 const path = require('path');
 
 //if (process.env.NODE_ENV !=='production') require('dotenv').config({path:'./.env'});
-if (process.env.NODE_ENV !=='production') require('dotenv').config();
+if (process.env.NODE_ENV !=='production') require('dotenv').config({path:'./.env'});
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(cors());
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__darname, 'client/buid')));
+    app.use(express.static(path.join(__darname, 'client/build')));
 
     app.get('*', function(req, res){
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
